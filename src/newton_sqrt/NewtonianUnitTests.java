8d37d0006906a8a6b4b7b9da5fbb9efcb5fc7135
shinabarger <davidshinabarger@gmail.com>
2017-09-07 20:29:01 -0400

    another unit test

--- src/newton_sqrt/NewtonianUnitTests.java
+++ src/newton_sqrt/NewtonianUnitTests.java
@@ -11,9 +11,16 @@ import static org.junit.Assert.assertEquals;
 public class NewtonianUnitTests {
 
     @Test
-    public void firstTest() {
+    public void checksSquareRootOf4Returns2() {
         double expected = 2.0;
         double actual = GetNewtonsMethod.getSquareRoot(4);
+        assertEquals(expected, actual, 0.000001);
+    }
+
+    @Test
+    public void squareRootOf16Returns4() {
+        double expected = 4.0;
+        double actual = GetNewtonsMethod.getSquareRoot(16);
         assertEquals(expected, actual, 0.000001);
     }
 
